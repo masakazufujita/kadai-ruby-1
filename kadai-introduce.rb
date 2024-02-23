85f8f91b2a878f95639f0244995a00eb346195f2
fujitakeiei <masakazu777@ymail.ne.jp>
2024-02-23 15:15:10 +0000

    別々の変数に代入

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,2 +1,7 @@
-profile = ['藤田','雅三',56]
-puts profile[0] + profile[1] + "です。" + profile[2] .to_s + "歳です。"
+# profile = ['藤田','雅三',56]
+# puts profile[0] + profile[1] + "です。" + profile[2] .to_s + "歳です。"
+
+myoji = '藤田'
+name = '雅三'
+age = 56
+puts myoji + name + "です。" + age.to_s + "歳です。"
