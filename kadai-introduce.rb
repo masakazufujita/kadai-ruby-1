4ab832ee558f3c4be1ce889312329b7219d729ed
fujitakeiei <masakazu777@ymail.ne.jp>
2024-02-23 08:58:35 +0000

    nameからprofileへ変更

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,6 @@
-name = {
+profile = {
   myoji: '藤田',
   namae: '雅三',
   age: 56
 }
-puts name[:myoji] + name[:namae] + "です。" + name[:age].to_s + "歳です。"
+puts profile[:myoji] + profile[:namae] + "です。" + profile[:age].to_s + "歳です。"
